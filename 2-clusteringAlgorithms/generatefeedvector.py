bdc427498346986b8aa4b979cdfa5bf5c9462512
Mukund Vemuri <mkv102@hotmail.com>
2018-10-08 01:40:11 -0400

    generating feed matrix

--- 2-clusteringAlgorithms/generatefeedvector.py
+++ 2-clusteringAlgorithms/generatefeedvector.py
@@ -7,10 +7,10 @@ def getwordcounts(url):
     wc = {}
 
     for e in d.entries:
-        if "summary" in e: sumary = e.summary
+        if "summary" in e: summary = e.summary
         else: summary = e.description
 
-        words = getwords(e.title + " " + summary)
+        words = getwords(summary)
         for word in words:
             wc.setdefault(word, 0)
             wc[word] += 1
@@ -25,12 +25,31 @@ def getwords(html):
 
 apcount = {}
 wordcounts = {}
+feedlist = []
 for url in open('data/feedlist.txt'):
+    feedlist.append(url)
     title, wc = getwordcounts(url)
     wordcounts[title] = wc
     for word, count in wc.items():
         apcount.setdefault(word, 0)
         if count > 1:
             apcount[word] += 1
-    
-    
+
+wordlist = []
+
+for w, bc in apcount.items():
+    frac = float(bc)/len(feedlist)
+    if frac > 0.1 and frac < 0.5: wordlist.append(w)
+
+out = open('data/blogdata.txt', 'w')
+out.write('Blog')
+for word in wordlist:
+    out.write('\t%s' % word)
+out.write('\n')
+for blog, wc in wordcounts.items():
+    #blog = blog.encode('ascii', ignore)
+    out.write(blog)
+    for word in wordlist:
+        if word in wc: out.write('\t%d' % wc[word])
+        else: out.write('\t0')
+    out.write('\n')
